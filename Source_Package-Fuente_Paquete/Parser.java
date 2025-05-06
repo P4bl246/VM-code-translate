5d3f5459f8b6f5342ed3b0e0e088dc4cec1fc326
P4bl246 <140914481+P4bl246@users.noreply.github.com>
2025-05-06 15:51:10 +0000

    d

--- Source_Package-Fuente_Paquete/Parser.java
+++ Source_Package-Fuente_Paquete/Parser.java
@@ -1,7 +1,7 @@
 package VM_code_Translator;
 
 import java.io.File;
-
+imtport java.nio.File;
 
 public class Parser {
     /*
@@ -11,8 +11,14 @@ public class Parser {
     public static int parsing(char input[]);// Constructor for the parsing
                                 // Constructor para el análisis sintáctico
     Files Preparation---------------------------------------------------------------------------------
-    public int CleanFile( file_In);// Clean the file of the spaces and void lines and comments(internal method)
+    public int CleanFile(File file_In);// Clean the file of the spaces and void lines and comments(internal method)
                                          // Limpiar el archivo de los espacios y líneas vacias y comentarios(Método interno)
+
+    public int CreateANewFile(String nameFile);// This method is used to create a new file(internal method)
+                                              // Este método se utiliza para crear un nuevo archivo(Método interno)
+
+    protected int File_to_txt(File file_In);// This method is used to convert the file to txt format(internal method)
+                                            // Este método se utiliza para convertir el archivo a formato txt(Método interno)
     public int PrepareFiles();
 
 
@@ -37,4 +43,38 @@ public class Parser {
         
         return 0;
     }
+
+//STARTS THE PROCESS OF PREPARE FILES (INICIA EL PROCESO DE PREPARACIÓN DE ARCHIVOS)----------------------------------------------------------------- 
+
+protected int File_to_txt(File file_In) {
+    // Create a new file, call "file.txt"
+    // Crear un nuevo archivo, llamado "file.txt"
+    int n = CreateANewFile("file.txt");
+    if (n != 0){
+        return 1; // Error case-En caso de error
+    }
+    // open the new file in write mode and the file_In in read mode
+    // abrir el nuevo archivo en modo escritura y el file_In en modo lectura
+    
+    return 0;
+}
+//-----------------------------------------------------------------------------
+public int CreateANewFile(String nameFile){
+    File file = new File(nameFile);
+try {
+    if (file.createNewFile()) {
+        System.out.println("File create.\n");
+        return 0;
+    } else {
+        System.out.println("The file exist.\n");
+        return 0;
+    }
+} catch (IOException e) {
+    System.out.printf("ERROR to try to create the file: %s\n", new String(nameFile));
+    return 1;
+}
+}
+
+public int CleanFile(File file_In){
+}
 }
