b86c11e797515611f4899f00afbdff6771bfa5f0
P4bl246 <140914481+P4bl246@users.noreply.github.com>
2025-05-10 17:27:59 +0000

    d

--- SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
+++ SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
@@ -1,5 +1,6 @@
 package AuxClass.sintax_parsing;
-
+import AuxClass.Parser.Parser; // Import the Parser class from the AuxCLass.Parser package
+//                                // Importar la clase Parser del paquete AuxCLass.Parser
 public class sintax_parsing {
 // This class is used to parse the file and check the syntax
 // Esta clase se utiliza para analizar el archivo y verificar la sintaxis
