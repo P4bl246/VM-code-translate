80387bbb75277600c37da9ba523697a44426e54e
P4bl246 <140914481+P4bl246@users.noreply.github.com>
2025-05-10 18:42:53 +0000

    d

--- SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
+++ SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
@@ -1,6 +1,8 @@
 package AuxClass.sintax_parsing;
 import AuxClass.Parser.Parser; // Import the Parser class from the AuxCLass.Parser package
-//                                // Importar la clase Parser del paquete AuxCLass.Parser
+                            // Importar la clase Parser del paquete AuxCLass.Parser
+import java.util.ArrayList; // Import the ArrayList class from the java.util package
+                            // Importar la clase ArrayList del paquete java.util
 public class sintax_parsing {
 // This class is used to parse the file and check the syntax
 // Esta clase se utiliza para analizar el archivo y verificar la sintaxis
@@ -21,11 +23,26 @@ public  int parsing(String input) {
     }
 //-----------------------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------
-//Espcial constructor for hasshing table
-// Constructor especial para la tabla de hash
-public Hashing (arrayList<String> list) {
-    
+//Espcial constructor for hasshing table similar to the Factory object
+// Constructor especial para la tabla de hash similar al objeto de fábrica
+public void Hashing (ArrayList<String> list) {
+    // Recorrer el arreglo de cadenas
+    // Iterate through the array of strings
+    for(String elemento : list){
+        hash(elemento);
+    }
 }
+public int hash(String key) {
+    return 0;
+}
+public int SearchinHash(String key) {
+    return 0;
+}
+public int InsertinHash(String key) {
+    return 0;
+}
+
+//-----------------------------------------------------
 public int Arthmetic_Expression(String input) {
     
     return 0;
