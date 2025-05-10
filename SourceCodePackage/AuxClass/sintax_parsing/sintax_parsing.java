28b3a0418cb0dd1297424d000a39ba3877ac7db8
P4bl246 <140914481+P4bl246@users.noreply.github.com>
2025-05-10 22:35:26 +0000

    d

--- SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
+++ SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
@@ -9,53 +9,37 @@ public class sintax_parsing {
 /* 
 //FUNCTIONS FOR PARSING SINTAX (FUNCIONES PARA EL ANÁLISIS SINTÁCTICO)----------------------------------------------------------------------------
 
-    public int parsing(String input);// This method is used to parse the input file
-                                                // Este método se utiliza para analizar el archivo de entrada
+    
     
     public int Arthmetic_Expression(String input);// This method is used to parse the arithmetic expression
                                                 // Este método se utiliza para analizar las expresión aritmética
 
+    public String GetNchars(String input, int n);// This method is used to get the first n characters of the input string
+                                                // Este método se utiliza para obtener los primeros n caracteres de la cadena de entrada
+
+    public int parsing(String input);// This method is used to parse the input file, integrating funcions
+                                    // Este método se utiliza para analizar el archivo de entrada, integrando funciones
 //END OF FUNCTIONS FOR PARSING SINTAX (FIN DE LAS FUNCIONES PARA EL ANÁLISIS SINTÁCTICO)--------------------------------------------------------------
 */
 //FUNCTIONS FOR PARSING SINTAX (FUNCIONES PARA EL ANÁLISIS SINTÁCTICO)----------------------------------------------------------------------------
 public  int parsing(String input) {
         return 0;
     }
-//-----------------------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------
-//Espcial constructor for hasshing table similar to the Factory object
-// Constructor especial para la tabla de hash similar al objeto de fábrica
-public hashTable(int size){
-    this.size = size;
-    this.table = new ArrayList[size];
-    for (int i = 0; i < size; i++) {
-        table[i] = new ArrayList<>();
-    }
-}
-public void Hashing (String list, int sizeofTable) {
-
-       int  hash_key = hash(list, sizeofTable);
-
-    
-}
-public int hash(String key, int n) {
-    int h = Integer.parseInt(key);
-    h = h % n;
-    return h;
-}
-public int SearchinHash(String key) {
-    
-    return 0;
-}
-public int InsertinHash(String key, int newSizeofTable) {
-    Hashing(key, newSizeofTable);
-    return 0;
-}
-
-//-----------------------------------------------------
+//------------------------------------------------------
 public int Arthmetic_Expression(String input) {
-    
     return 0;
+}
+//------------------------------------------------------
+public void makeHashTablePreDet(ArrayList<String> arrayString){
+    
+}
+//-------------------------------------------------------
+public String GetNchars(String input, int n) {
+    String result = "";
+    for (int i = 0; i < n; i++) {
+        result += input.charAt(i);
+    }
+    return result;
 }
 //END THE PROCESS OF PARSING SINTAX (TERMINA EL PROCESO DE ANÁLISIS SINTÁCTICO)-----------------------------------------------------------------
 
