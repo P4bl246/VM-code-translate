0944ca44d3aee280f89239e2f41ec7fcba4e01e9
P4bl246 <140914481+P4bl246@users.noreply.github.com>
2025-05-11 03:01:43 +0000

    d

--- SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
+++ SourceCodePackage/AuxClass/sintax_parsing/sintax_parsing.java
@@ -11,15 +11,16 @@ public class sintax_parsing {
 //FUNCTIONS FOR PARSING SINTAX (FUNCIONES PARA EL ANÁLISIS SINTÁCTICO)----------------------------------------------------------------------------
 
     
-    
+//-----------------------------------****************--------------------------******   
     public int Arthmetic_Expression(String input);// This method is used to parse the arithmetic expression
                                                 // Este método se utiliza para analizar las expresión aritmética
+
+    public void CreateHashTable(String element, int SimpleOrMultiples, ArrayList<String> NewElements, HashMap<String, Integer> hashTable);// This method is used to create a hash table
+//-----------------------------------****************--------------------------******                                                                                                                                    // Este método se utiliza para crear una tabla hash
 
     public String GetNchars(String input, int n);// This method is used to get the first n characters of the input string
                                                 // Este método se utiliza para obtener los primeros n caracteres de la cadena de entrada
 
-    public void CreateHashTable(String element, int SimpleOrMultiples, ArrayList<String> NewElements, HashMap<String, Integer> hashTable);// This method is used to add elements to the hash table
-                                                                                                                                    // Este método se utiliza para agregar elementos a la tabla hash
 //END OF FUNCTIONS FOR PARSING SINTAX (FIN DE LAS FUNCIONES PARA EL ANÁLISIS SINTÁCTICO)--------------------------------------------------------------
 */
 //FUNCTIONS FOR PARSING SINTAX (FUNCIONES PARA EL ANÁLISIS SINTÁCTICO)----------------------------------------------------------------------------
@@ -32,19 +33,28 @@ public int Arthmetic_Expression(String input) {
     preDet.add("add");
     preDet.add("sub");
     preDet.add("neg");
-    preDet.add("eq");
-    preDet.add("gt");
-    preDet.add("lt");
-    preDet.add("and");
-    preDet.add("or");
-    preDet.add("not");
-
+    
     HashMap<String, Integer> hashTable = new HashMap<>();
     CreateHashTable(null, 1, preDet, hashTable);
-
-    // Ejemplo: buscar el valor hash de una palabra
-    System.out.println("Hash de 'add': " + hashTable.get("add"));
-
+    
+    if(input != null) {
+        String element = GetNchars(input, 3);
+        if (hashTable.containsKey(element)) {
+            Parser parser = new Parser();
+            parser.setInput(input);
+            parser.setHashTable(hashTable);
+            parser.parse();
+            return 1;
+        } else {
+            System.out.println("Error: Invalid arithmetic expression");
+            return -1;
+        }
+    } else {
+        System.out.println("Error: Input is null");
+        return -1;
+    }
+    // If the input is null, return -1
+    // Si la entrada es nula, devuelve -1
     return 0;
 }
 
@@ -52,7 +62,11 @@ public int Arthmetic_Expression(String input) {
 
 public void CreateHashTable(String element, int SimpleOrMultiples, ArrayList<String> NewElements, HashMap<String, Integer> hashTable) {
     int hash;
+    // If SimpleOrMultiples is not 0, create a hash table for multiple elements
+    // Si SimpleOrMultiples no es 0, crea una tabla hash para múltiples elementos
     if (SimpleOrMultiples != 0) {
+        // Iterate through the list of new elements and add them to the hash table
+        // Itera a través de la lista de nuevos elementos y agrégales a la tabla hash
         for (String element2 : NewElements) {
             hash = 0;
             for (int i = 0; i < element2.length(); i++) {
@@ -60,13 +74,17 @@ public void CreateHashTable(String element, int SimpleOrMultiples, ArrayList<Str
             }
             hashTable.put(element2, hash);
         }
-    } else {
+    }
+    // If SimpleOrMultiples is 0, create a hash table for a single element
+    // Si SimpleOrMultiples es 0, crea una tabla hash para un solo elemento 
+    else {
         hash = 0;
         for (int i = 0; i < element.length(); i++) {
             hash += element.charAt(i);
         }
         hashTable.put(element, hash);
     }
+    return;
 }
 //-------------------------------------------------------
 public String GetNchars(String input, int n) {
